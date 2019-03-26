0fed90c1b3420353484f6d4dabb3bdd82f6ef60a
MadLad <41120880+MegaDoot@users.noreply.github.com>
2019-03-26 13:23:50 +0000

    Update dunder_dec.py

--- dunder_dec.py
+++ dunder_dec.py
@@ -1,12 +1,17 @@
 class Derivative(object):
     def __init__(self, num):
         self.num = num
+    
+    def mul(self, other):
+        return self.num * other.num
 
 def dry_create(cls, var_name, *funcs):
     for func in funcs:
-        setattr(cls, func, lambda self, other: [print(f"Function {func} on self.{var_name}, other.{var_name}")])
+        setattr(cls, func, lambda self, other: getattr(self, var_name).__add__(getattr(other, var_name)))
+        #i.e. self.num.__add__(other.num)
     return cls
 
-Derivative = dry_create(Derivative, "num", "foo")
+Derivative = dry_create(Derivative, "num", "__add__")
 
-test = Derivative(5)
+test1 = Derivative(2)
+test2 = Derivative(3)
