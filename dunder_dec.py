7d824ccf85797c1af005ff0e4e6af1495085a24c
MadLad <41120880+MegaDoot@users.noreply.github.com>
2019-03-29 22:30:54 +0000

    Fully working, 1 or 2 arguments

--- dunder_dec.py
+++ dunder_dec.py
@@ -1,6 +1,26 @@
-import functools
+"""
+Alex Scorza 2019
+"""
 
-class add_func(object):
+from inspect import signature as sig
+
+def _find_args(func):
+    return len(sig(func).parameters)
+
+def _make_func(this, other, func:str, var_name:str): #__add__, num
+    function = getattr(getattr(this, var_name), func) #Retrive function to use dunder method: this.var_name.func
+    args_num = _find_args(function) #Number of extra arguments taken (i.e. __add__ would be 1, __iter__ would be 0)
+    if args_num == 1: #If binary
+        return function(getattr(other, var_name))
+    elif args_num == 0: #If unary
+        return function() #No arguments needed
+    else:
+        raise DunderArgsError("Accepting only 1 or 2 arguments") #As far as know they all accept 1 or 2
+
+class DunderArgsError(TypeError): #Custom exception
+    pass
+
+class add_methods(object):
     def __init__(self, var_name, *funcs):
         self.var_name = var_name
         self.funcs = funcs
@@ -8,21 +28,24 @@ class add_func(object):
     def __call__(self, cls):
         class Decorated(cls):
             for func in self.funcs:
-                print(func)
-                setattr(cls, func, lambda this, other, func = func: getattr(getattr(this, self.var_name), func)(getattr(other, self.var_name)))
+                setattr(cls, func, lambda this, other = None, func = func: _make_func(this, other, func, self.var_name))
                 #i.e. self.num.__add__(other.num)
                 #Note that 'this' is used to distinguish from 'self' defined in the class
         Decorated.__name__ = cls.__name__
         return Decorated
-    
 
-@add_func("num", "__add__", "__mul__")
-class Derivative(object):
-    def __init__(self, num):
-        self.num = num
+__all__ = ("add_func",)
 
 if __name__ == "__main__":
+    @add_methods("num", "__add__", "__mul__")
+    @add_methods("array", "__iter__")
+    class Derivative(object):
+        def __init__(self, num):
+            self.num = num
+            self.array = (1, 2, 3)
+
     test1 = Derivative(2)
     test2 = Derivative(3)
     print(test1 + test2)
     print(test1 * test2)
+    print([n for n in test1])
