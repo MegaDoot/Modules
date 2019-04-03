f92fe1a9747906d0d3ff03b0ee9edf6cfdd1e9f9
Alex <16scorzaa@cedarsupper.co.uk>
2019-04-03 21:59:15 +0100

    Splits and strips
    Removes spaces if not in quotations and splits based on semicolons if not in quotations

--- tokenise.py
+++ tokenise.py
@@ -9,6 +9,7 @@ regexps = (r"^\s*", r"^\w+", r"^\s*-\s*")
 args_re = (r"^\b+", r"^\s*:\s*")
 
 def tokenise(string):
+    
     parts = []
     for regex in regexps:
         found = re.findall(regex, string)
@@ -16,20 +17,43 @@ def tokenise(string):
         string = string[len(parts[-1]):]
     print(parts)
     print(string)
-    brackets = 0
-    for char in string:
-        if char in opening:
-            brackets += 1
-        elif chars in closing:
-            brackets -= 1
+    not_quoted = out_quotes(string) #Remove all text in quotation marks and apostrophes
+    print(not_quoted)
+    stripped = "" #Spaces removed
+    for i in range(len(string)):
+        if string[i] == " ":
+            if not i in not_quoted:
+                stripped += string[i]
+        else:
+            stripped += string[i]
+    string = stripped
+    not_quoted = out_quotes(string) #Needs to re-calculate
+
+    sep_positions = []
+    for pos in not_quoted:
+        if string[pos] == ";":
+            sep_positions.append(pos)
+    print(sep_positions)
+    split = [""]
+    for i in range(len(string)):
+        if i in sep_positions:
+            split.append("")
+        else: #Skips semicolons
+            split[-1] += string[i]
+
+    print(split)
+
+    
 
 def out_quotes(string):
-    positions = []
-    for i in range(len(string)):
-        if string[i] == '"':
-            positions.append(i)
-    print(positions)
+    print("max =", len(string))
+    indexes = [i for i in range(len(string)) if string[i] == '"']
+    inside = []
+    iterate = tuple(range(0, len(indexes), 2))
+    for i in iterate:
+        inside += tuple(range(indexes[i], indexes[i + 1] + 1))
+    outside = [i for i in range(len(string)) if not i in inside]
+    return outside
 
-out_quotes('[char + "hi" for char in "hello"]')
-
-#tokenise("obj - type(obj): obj.num; int, float: obj")
+if __name__ == "__main__":
+    tokenise('obj - type(obj): obj.num; int, float: obj; str: "hello there,"  + obj')
