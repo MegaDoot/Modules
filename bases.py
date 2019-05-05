f4b490f9ec8c57130f4975b1baf3604d77125be0
MadLad <41120880+MegaDoot@users.noreply.github.com>
2019-05-05 10:10:08 +0100

    Restored

--- bases.py
+++ bases.py
@@ -1,296 +1,66 @@
 """
-Alex Scorza 2019
-Copyright statement:
-1.
-    1. Plz dont steal
-
-
-HOW TO USE 'construct'
-Avoids doing something like this:
-(Code Example 1)
------------
-    def __init__(genus, species, legs, mass, speed, eyes = 2):
-        self.genus = genus
-        self.species = species
-        self.legs = legs
-        self.mass = mass
-        self.speed = speed
-        self.eyes = eyes
-        print("Initialised")
------------        
-BECAUSE THAT WOULD BE  W E T
-Instead, you can do:
------------
-    @construct(args = ("genus", "species", "legs", "mass", "speed"), kwargs = [("eyes", 2)])
-    class Foo:
-        def __init__(self):
-            print("Initialised")
------------
-W O W
-ISN'T
-THAT
-D          R          Y
-
-
-HOW TO USE 'add_method':
-Add a decorator before the function -
-(Code Example 1)
------------
-    import dryclass as dc
-    @dc.add_methods("__main__.Foo: obj.num", "__add__", "__sub__")
-    class Foo:
-        def __init__(self):
-            self.num = 5
-    print(repr(Foo() + Foo())) #Returns 10
------------
-This gets the class and adds each of the specified magic methods. An unlimited amount is allowed.
-The lambda at the starts specifies which attribute of the class to get.
------------
-Here is a usage of the function on the result (wrapper) and the function called on each argument:
-(Code Example 2)
------------
-    @dc.add_methods("__main__.Foo: int(obj.num)", "__add__", "__sub__", "__mul__", "__floordiv__", wrapper = "str")
-    class Foo:
-        def __init__(self):
-            self.num = "5"
-    print(repr(Foo() + Foo())) #Returns "10"
------------
-Here, the value of num is always a string so it is converted to an integer, the calculations are
-done and the result is converted back into a string, so "5" + "6" would become "11" in this case,
-despite being strings. The equivalent of the code above/code example 2 done normally:
-(Code Example 3)
------------
-    class Foo:
-        def __init__(self):
-            self.num = "5"
-            
-        def __add__(self, other):
-            return str(int(self.num) + int(other.num))
-            
-        def __mul__(self, other):
-            return str(int(self.num) * int(other.num))
-            
-        def __sub__(self, other):
-            return str(int(self.num) - int(other.num))
-            
-        def __floordiv__(self, other):
-            return str(int(self.num) // int(other.num))
-    print(repr(Foo() + Foo())) #Returns "10"
------------
-As you can see, this code is very WET, repetitive and time-consuming to edit.
------------
-Now for the mysterious syntax at the start of each decorator. This string defines the statement that
-should be applied to each different data type. A string is used over a dictionary for 3 reasons:
--Making different data types have the same statement is easier
--More freedom
--Improved readability and writability
-In the example below, both 'float' and 'int' have the same behaviour so we do
-'float, int: obj' instead of 'float: obj, int: obj' because that would be  W E T
-It's important to note that anything defined within the program would not be
-available to the module, hence the need to write '__main__' beforehand. Also,
-each section (made up of data types separated by commas, a colon and a statement)
-is separated by a SEMICOLON and not a comma.
-(Code Example 4)
------------
-    @dc.add_methods("__main__.Foo: float(obj.num); float, int: obj", "__add__", "__radd__")
-    class Foo:
-        def __init__(self):
-            self.num = 5
-    print(repr(Foo() + 1), repr(Foo() + Foo()), repr(3 + Foo()))
------------
-
-HOW TO USE 'importer':
-What if you don't want to add '__main__' before all functions you define?
-The 'importer' function allows you to write your own statement, i.e.
-'importer("m")', so you can now write it as m.Foo not __main__.Foo
-'importer("m")' is equivalent to 'import __main__ as m'
+Note the following:
+Decimal places are not allowed because I am lazy.
+Not all operators are used
+Consider yourselves honoured that I took the time to add augmented assigment - 'ctrl+c', 'ctrl+v' and 'i' take a lot of effort
 """
-
-__all__ = ("construct", "add_methods", "importer", "BIN_OPERS", "IBIN_OPERS")
-
-def construct(args = [], kwargs = []):
-    def decorator(cls):
-        if hasattr(cls, "__init__"):
-            func = cls.__init__
-        else:
-            func = lambda self: None
-        def init_wrapper(*a, **kw):
-            self = a[0]
-            a = a[1:] #Save and remove 'self' argument
-            if len(a) < len(args) or len(a) > len(args) + len(kwargs):
-                raise TypeError("Got {} args, expected {} to {}".format(len(a), len(args), len(args) + len(kwargs)))
-            remove_kw = 0 #Ignore all that were entered as args
-            for i in range(len(a)): #arguments
-                if i + 1 > len(args):
-                    set_val = kwargs[1 + i - len(a)][0]
-                    remove_kw += 1
-                    setattr(cls, set_val, a[i])
-                else:
-                    setattr(cls, args[i], a[i])
-            for k, v in kwargs[remove_kw:]: #All kwargs entered in the keyword and argument format
-                if k in kw:
-                    setattr(cls, k, kw[k])
-                else:
-                    setattr(cls, k, v)
-            func(self)
-        setattr(cls, "__init__", init_wrapper)
-        return cls
-    return decorator
-
-
-
-def remove(iterable, name):
-    array = iterable[:]
-    del array[array.index(name)]
-    return array
-
-BIN_OPERS = ("__add__", "__div__", "__floordiv__", "__mod__", "__mul__", "__pow__", "__sub__", "__truediv__")
-IBIN_OPERS = ("__iadd__", "__idiv__", "__imod__", "__imul__", "__pow__", "__isub__")
-
-##IBIN_OPERS = tuple(map(lambda func: "__i" + func[2:], BIN_OPERS))
-
-class Tkn:
-    @staticmethod
-    def split_with(string, delimiter):
-        not_quoted = Tkn.out_quotes(string)
-        sep_positions = []
-
-        for pos in not_quoted:
-            if string[pos] == delimiter:
-                sep_positions.append(pos)
-
-        split = [""]
-        for i in range(len(string)):
-            if i in sep_positions:
-                split.append("")
-            else: #Skips semicolons
-                split[-1] += string[i]
-        return split
-
-    @staticmethod
-    def tokenise(string):
-        not_quoted = Tkn.out_quotes(string) #Remove all text in quotation marks and apostrophes
-        stripped = "" #Spaces removed
-        for i in range(len(string)):
-            if string[i] == " ":
-                if not i in not_quoted:
-                    stripped += string[i]
-            else:
-                stripped += string[i]
-        string = stripped
-
-        separated = Tkn.split_with(string, ";")
-        for i in range(len(separated)):
-            separated[i] = Tkn.split_with(separated[i], ":")
-            separated[i][0] = list(Tkn.split_with(separated[i][0], ","))
-        return separated
-
-    @staticmethod
-    def method(multi_key_dict, obj, key):
-        type_ = type(obj)
-        for k_tuple in multi_key_dict.keys():
-            evaluated = tuple(map(eval, k_tuple))
-            if type_ in evaluated:
-                return multi_key_dict[k_tuple]
-        raise KeyError("Key '{}' not found".format(key))
-
-    @staticmethod
-    def out_quotes(string):
-        indexes = [i for i in range(len(string)) if string[i] == '"']
-        inside = []
-        iterate = tuple(range(0, len(indexes), 2))
-        for i in iterate:
-            inside += tuple(range(indexes[i], indexes[i + 1] + 1))
-        outside = [i for i in range(len(string)) if not i in inside]
-        return outside
-
-    @staticmethod
-    def unpack(tokens):
-            individual = {}
-            for i in range(len(tokens)):
-                for x in range(len(tokens[i][0])):
-                    individual[tokens[i][0][x]] = tokens[i][1]
-            return individual
-
-    @staticmethod
-    def run(dict_, obj):
-        for k, v in dict_.items():
-            if eval(k) == type(obj):
-                return eval(v)
-        raise NameError("Behaviour of type '{}' is not defined".format(type(obj).__name__))
-
-def dunders(obj): #Lists all dunder methods of an object
-    return tuple(filter(lambda func: func.startswith("__") and func.endswith("__"), dir(obj)))
-
-def make_func(this, func, evaluator, wrapper, *args):
-##    print("\nCalled")
-    evaluated = evaluator(this)
-    if evaluated == this: #If unchanged, it finds its own method, resulting in recursion :(
-        raise RecursionError("Statement entered results in recursion - statement type of class to be decorated must not equal 'obj'")
-##    print("Amount:", extra_args)
-    
-    to_call = getattr(evaluated, func)
-##    print("Type:", type(to_call))
-##    print("Will call:", to_call)
-    wrapper = eval(wrapper)
-    return wrapper(to_call(*map(evaluator, args)))
-
-class DunderArgsError(TypeError): #Custom exception
-    pass
-
-class NoSignatureError(ValueError): #Custom exception
-    pass
-
-def importer(name = "__main__"):
-    globals()[name] = __import__("__main__")
-
-class add_methods(object): #The decorator
-    def __init__(self, syntax, *funcs:str, wrapper = "lambda value: value"):
-        self.func_dict = Tkn.unpack(Tkn.tokenise(syntax))
-##        print(self.func_dict)
-        self.funcs = funcs
-        self.wrapper = wrapper
-    
-    def __call__(self, cls):
-        evaluator = lambda obj: Tkn.run(self.func_dict, obj)
-##        class Decorated(cls): #Temporary decorated class that contains all of the
-        for func in self.funcs: #func is a string
-##            print("Iterated with", func)
-            code = lambda this, *args, func = func: make_func(this, func, evaluator, self.wrapper, *args)
-            setattr(cls, func, code)
-                #i.e. self.num.__add__(other.num)
-                #Note that 'this' is used to distinguish from 'self' defined in the class
-        return cls #Turn the class into the decorated class
-
-
+import dryclass as dc
+behaviour = "__main__.Base: __main__.to_ten(obj.num, obj.base)"
+@dc.add_methods(behaviour, *dc.BIN_OPERS)
+@dc.add_methods(behaviour, *dc.IBIN_OPERS, wrapper = "Base.__init__")
+class Base:
+    def __init__(self,num,base = 10): #ACCEPTS 1 OR 2 ARGUMENTS, base defaults to 10
+        self.num = str(num)
+        self.base = base
+        check(self.num,self.base)
+    def in_base(self,to): #Displays in base, for user/not referenced
+        return from_ten(to_ten(self.num,self.base),to)
+    def to_base(self,to): #Uses self.num and self.base, changes to base
+        self.num = self.in_base(to)
+        self.base = to
+        check(self.num,self.base) #Ensure that it only contains the correct characters
+    #Note that any magic/dunder methods will return base 10 number/decimal
+chars = "0123456789abcdefghijklmnopqrstuvwxyz" #Up to base 36
+#Magic methods return new 'Base' object
+def check(num,base):
+    num = str(num)  
+    if num[0] == "-": #Remove leading '-'
+        num = num[1:]
+    if not base in range(2,len(chars)+1):
+        raise ValueError("Base of {} is invalid - must be between (and inclusive of 2 and {})".format(base,len(chars)))
+    for x in str(num):
+            if x not in chars or chars.index(x) > base:
+                raise ValueError("Invalid character used: '{}' for base {}".format(str(x),base))
+def to_ten(num,base):#Could be done in one line
+    neg = 0
+    if num[0] == "-":
+        num = num[1:]
+        neg = 1
+    result = 0
+    for n in reversed(range((len(str(num))))):
+        result += chars.index((str(num)[len(str(num))-n-1]).lower())*(base**n)
+        #'chars.index' to account for letters - numbers > 9 are letters
+    if neg == 0:
+        return result
+    else:
+        return -result 
+def from_ten(num,base): #Returns string as there may be letters
+    num = int(num)
+    values = []
+    if num < 0:
+        values += "-"
+    n = 0
+    while base**(n+1) < num: #Stops 1 before so it doesn't become too large
+        n += 1
+    for x in reversed(range(n+1)):#Must include last number
+        values += [num//(base**x)] #Integer division and carry the remainder
+        num %= base**x #Becomes remainder
+    return "".join(chars[x] for x in values) #'chars[x]' not 'int(x)' in case of letter
 if __name__ == "__main__":
-    #For construct
-    @construct(args = ("a", "b"), kwargs = (("c", 3), ("d", 4), ("e", 5)))
-    class Foo:
-        def __init__(self):
-            print("Initialised")
-            print(self.a, self.b, self.c, self.d, self.e)
-    
-    test = Foo(1, 2, 9, e = 10)
-    
-    #For add_methods
-    @add_methods("Derivative: obj.array; int: obj", "__len__", "__iter__", "__getitem__", "__setitem__")
-    @add_methods("Derivative: int(obj.num); int, float: obj", "__add__", "__mul__", wrapper = "str")
-    class Derivative(object):
-        def __init__(self, num, array, string):
-            self.num = num
-            self.array = array
-            self.string = string
-    print()
-    test1 = Derivative("3", [1, 2, 3], "hello")
-    test2 = Derivative("10", [1, 3, 3, 7], "hi")
-    print(len(test2))
-    print(repr(test1 + 5))
-    print(repr(test1 + test2))
-    print(repr(test1 * test2))
-    print(repr(test2[-1]))
-    print("Before:", list(test1))
-    test1[0] = 100
-    print("After:", list(test1))
-else:
-    importer()
+    a = Base("-f3",16) #-243
+    b = Base("-10101",2) #-21
+    c = Base(a-b) #Defauts to base 10
+    print(c.num)
+    c /= Base("2")
+    print(c.num)
+    print(Base("f",16) == Base("15",10))
