f95c514b139a871310d5d7f6a995b222db6dd346
MadLad <41120880+MegaDoot@users.noreply.github.com>
2019-05-05 12:43:00 +0100

    Works with negative numbers properly

--- bases.py
+++ bases.py
@@ -2,17 +2,20 @@
 Note the following:
 Decimal places are not allowed because I am lazy.
 Not all operators are used
-Consider yourselves honoured that I took the time to add augmented assigment - 'ctrl+c', 'ctrl+v' and 'i' take a lot of effort
 """
 import dryclass as dc
-behaviour = "__main__.Base: __main__.to_ten(obj.num, obj.base)"
-@dc.add_methods(behaviour, *dc.BIN_OPERS)
-@dc.add_methods(behaviour, *dc.IBIN_OPERS, wrapper = "Base.__init__")
+dc.importer("m")
+behaviour = "m.Base: m.to_ten(obj.num, obj.base); float, int: int(obj)"
+@dc.add_methods(behaviour, *dc.BIN_OPERS, "__eq__")
+@dc.add_methods(behaviour, *dc.IBIN_OPERS, wrapper = "lambda value: m.Base(int(value))")
 class Base:
     def __init__(self,num,base = 10): #ACCEPTS 1 OR 2 ARGUMENTS, base defaults to 10
         self.num = str(num)
         self.base = base
         check(self.num,self.base)
+    def __repr__(self):
+        string = f"Base({self.num}, {self.base})"
+        return string + ("" if self.base == 10 else f" (equals {self.in_base(10)})")
     def in_base(self,to): #Displays in base, for user/not referenced
         return from_ten(to_ten(self.num,self.base),to)
     def to_base(self,to): #Uses self.num and self.base, changes to base
@@ -32,30 +35,32 @@ def check(num,base):
             if x not in chars or chars.index(x) > base:
                 raise ValueError("Invalid character used: '{}' for base {}".format(str(x),base))
 def to_ten(num,base):#Could be done in one line
-    neg = 0
+    neg = False
     if num[0] == "-":
         num = num[1:]
-        neg = 1
+        neg = True
     result = 0
     for n in reversed(range((len(str(num))))):
         result += chars.index((str(num)[len(str(num))-n-1]).lower())*(base**n)
         #'chars.index' to account for letters - numbers > 9 are letters
-    if neg == 0:
-        return result
+    if neg:
+        return -result
     else:
-        return -result 
+        return result 
 def from_ten(num,base): #Returns string as there may be letters
     num = int(num)
     values = []
+    negative = False
     if num < 0:
-        values += "-"
+        negative = True
+        num = abs(num)
     n = 0
     while base**(n+1) < num: #Stops 1 before so it doesn't become too large
         n += 1
     for x in reversed(range(n+1)):#Must include last number
         values += [num//(base**x)] #Integer division and carry the remainder
         num %= base**x #Becomes remainder
-    return "".join(chars[x] for x in values) #'chars[x]' not 'int(x)' in case of letter
+    return ("-" if negative else "") + "".join(chars[x] for x in values) #'chars[x]' not 'int(x)' in case of letter
 if __name__ == "__main__":
     a = Base("-f3",16) #-243
     b = Base("-10101",2) #-21
@@ -64,3 +69,4 @@ if __name__ == "__main__":
     c /= Base("2")
     print(c.num)
     print(Base("f",16) == Base("15",10))
+    print(Base("f",16) == Base("16",10))
