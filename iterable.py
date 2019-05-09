747ea141172a5540e5285bec508b6812531309e4
MadLad <41120880+MegaDoot@users.noreply.github.com>
2019-05-09 11:10:31 +0100

    Delete iterable.py

--- iterable.py
+++
@@ -1,8 +0,0 @@
-import inspect
-def is_iter(val):
-  if hasattr(val, "__iter__") and not inspect.isclass(val):
-    return True
-  return False
-if __name__ == "__main__":
-  while 1:
-   print(is_iter(eval(input("\n> "))))
