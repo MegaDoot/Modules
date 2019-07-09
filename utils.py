5fa23534f3812b71ed564556e922d4cb209a2f3a
Lucas <lucas.gathercole@gmail.com>
2019-07-09 14:39:31 +0100

    nested iterable unpacking

--- utils.py
+++ utils.py
@@ -11,3 +11,34 @@ def isiterable(obj):
         return True
     except TypeError:
         return False
+    
+def unpack_nested(seq, unpack_check = None):
+    if len(seq) == 1:
+        return seq[0]
+    results = []
+    for item in seq:
+        if (not unpack_check) or unpack_check(item):
+            try:
+                results += unpack_nested(item, unpack_check)
+                continue
+            except TypeError:
+                pass
+        results.append(item)
+    return results
+
+if __name__ == "__main__":
+    print("Unpack Nested:")
+    dataset = [
+        {
+            "name": "Any iterable",
+            "data": (["ab", [], [["b", "ded"], "c"]],),
+        }, {
+            "name": "Lists only",
+            "data": (["ab", [], [["b", "ded"], "c"]], lambda obj: isinstance(obj, list)),
+        }
+        
+    ]
+    for test in dataset:
+        print(str(test["name"])+":")
+        print(" Initial data:", ", ".join(str(item) for item in test["data"]))
+        print(" Result:", unpack_nested(*test["data"]))
